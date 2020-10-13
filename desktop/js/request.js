1e16a7312620125081b2d81118e3521bf249d7f8
Nejc Rebernik <nejc@nejcrebernik.si>
2020-10-13 23:43:20 +0200

    Form validation

--- desktop/js/request.js
+++ desktop/js/request.js
@@ -7,6 +7,23 @@ function send() {
         enrollmentId: $("#enrollmentId").val(),
         country: $("#country").val()
     }
-    
+    // validate input
+    let proceed = true;
+    for (let id of ["name", "surname", "email", "enrollmentId", "country"]) {
+        let elem = document.getElementById(id);
+        let valid = elem.reportValidity();
+        if (!valid) {
+            proceed = false;
+            break;
+        }
+    }
+    if (!proceed) {
+        return;
+    }
+    console.log(data);
+    // hide button and show loading
+    document.getElementById("ss").classList.add("d-none");
+    document.getElementById("loading").classList.remove("d-none");
+    // send request
     ipcRenderer.invoke("request", data);
 }
